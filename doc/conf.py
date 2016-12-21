b2afde78368d53584c158296f127363fa7b629b4
Brian Moss <kallimachos@gmail.com>
2016-12-21 17:13:47 +1000

    Use chios

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['bolditalic']
+extensions = ['chios.bolditalic']
 
 sphinxmark_enable = True
 
