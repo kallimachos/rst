9249dbd4916aece34d27d09fc2c287cadaa2b644
Brian Moss <kallimachos@gmail.com>
2017-03-29 09:49:02 +1000

    Add file for testing syntax

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sphinx experiments'
-copyright = u'2016, Brian Moss'
+copyright = u'2017, Brian Moss'
 author = u'Brian Moss'
 
 # The version info for the project you're documenting, acts as replacement for
---
+++ doc/syntax.rst
@@ -0,0 +1,5 @@
+======
+Syntax
+======
+
+.. remote-include:: https://raw.githubusercontent.com/rcbops/rpc-openstack/master/rpcd/etc/openstack_deploy/README.rst
